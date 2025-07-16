40cfb5ea56d0746cf4e38e35f7d5048a8ad27bbe
Harsh Sharma <Harshsharma29073@gmail.com>
2025-07-16 14:24:38 +0530

    enhance background and css features

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -41,7 +41,7 @@ export default function LandingPage() {
                 </div>
                 <div>
 
-                    <img src="/mobile.png" alt="" />
+                    <img src="https://imgs.search.brave.com/H0omDwDEjK7uAeNKO2eKNkhleADUTEKVNNHkoOCdXPk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zbS5w/Y21hZy5jb20vdC9w/Y21hZ19hdS9waG90/by9yL3JpbmdjZW50/cmEvcmluZ2NlbnRy/YWwtdmlkZW9fNDI5/dy44MDAucG5n" alt="hi" />
 
                 </div>
             </div>
